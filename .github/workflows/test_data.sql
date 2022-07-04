79997405207198693ae84c879e48e76f779f7966
Sercan Sensulun <sercan.sensulun@cloudmore.com>
2022-07-04 14:40:35 +0300

    populate test data from file.

---
+++ .github/workflows/test_data.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE test_database;
+
+use test_database; 
+
+CREATE TABLE Persons (PersonID int,Username varchar(255));
+INSERT INTO Persons (PersonID, Username) VALUES (1, 'Test_Username'):
+SELECT * FROM Persons
