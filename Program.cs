3734f8d61798f011b66c8636dcdb029fc3a39867
Sercan Sensulun <sercan.sensulun@cloudmore.com>
2022-07-04 17:41:14 +0300

    sample db query is added.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data.SqlClient;
+using System.Text;
 
 namespace minimal_db_poc
 {
@@ -7,6 +9,41 @@ namespace minimal_db_poc
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            try 
+            { 
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+                builder.DataSource = "localhost"; 
+                builder.UserID = "sa";            
+                builder.Password = "dbatools.I0";     
+                builder.InitialCatalog = "tempdb";
+         
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+                    
+                    connection.Open();       
+
+                    String sql = "SELECT name, collation_name FROM sys.databases";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                            }
+                        }
+                    }                    
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
